afef6b6da26105aaf5b17c36aa3db89f7e3e84fb
brenoq <brenoq@gmail.com>
2019-10-27 21:03:40 -0300

    update: Create student model and controller

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,27 @@
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email } = await Student.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
+}
+
+export default new StudentController();
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,17 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+
+import authMiddleware from './app/Middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/students', StudentController.store);
+
+routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
